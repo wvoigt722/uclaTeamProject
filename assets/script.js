a9f95fa7ca27b2e0f67cb623b479965e2dc6e8fd
Kayla Casale <kayla.casale@gmail.com>
2022-12-10 17:54:50 -0800

    wanting to pull

--- assets/script.js
+++ assets/script.js
@@ -13,6 +13,9 @@
 // // fetch(requestUrl)
 // //     .then()
 
+var img = document.createElement("img");
+img.src = "assets/real estate 1.jpeg"
+document.body.appendChild(img);
 
 // //* go to url desired
 // //* look at documentation require for property
@@ -132,8 +135,47 @@ function searchFunction() {
 document.querySelector('.searchBtn').addEventListener('click', searchFunction)
 
 
-function cityValuesSearch(city) {
-    fetch
+function cityValuesSearch(address) {
+    var container = document.querySelector(".container");
+
+    fetch('https://api.gateway.attomdata.com/propertyapi/v1.0.0/property/address?postalcode=82009&propertytype=RESIDENTIAL%20(NEC)&orderby=calendardate&page=1&pagesize=100', {
+        method: 'GET',
+        headers: {
+            'Accept': 'application/json',
+            'Content-Type': 'application/json',
+            'apikey': 'f274e5cbf40a2264853a5e68d2b0d5d2'
+        }
+    })
+        .then((res) => res.json())
+        .then((data) => {
+            for (var i = 0; i < data.property.length; i++) {
+                var createTableRow = document.createElement("tr");
+                var tableData = document.createElement("td");
+                var addressEl = document.createElement('p')
+
+                console.log(data.property[i].address);
+                var propertyAddressLine1 = data.property[i].address.line1;
+                var propertyAddressLine2 = data.property[i].address.line2;
+                console.log(propertyAddressLine1 + propertyAddressLine2);
+                var address = propertyAddressLine1 + propertyAddressLine2;
+                // var pEl = document.createElement("p");
+                // pEl.textContent = address
+                // container.append(pEl);
+                addressEl.textContent = address;
+
+                tableData.appendChild(addressEl);
+                createTableRow.appendChild(tableData);
+                container.appendChild(createTableRow);
+                //var propertyStreetNumber =
+                // for (var i = 0; i < property.length; i++) {
+                //     console.log('Successful POST request:', property[i].address);
+                // }
+            }
+            return data;
+        })
+        .catch((error) => {
+            console.error('Error in POST request:', error);
+        });
 }
 
 
