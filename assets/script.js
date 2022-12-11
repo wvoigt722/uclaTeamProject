23b2c8bf15d963d57f58810e6525c077c903e915
Owen Garrett <owengarrett@Owens-Air.lan>
2022-12-10 19:55:48 -0500

    added css html js

--- assets/script.js
+++ assets/script.js
@@ -1,21 +1,142 @@
-// // var request = new Request.Builder()
-// //     .url("https://api.gateway.attomdata.com/propertyapi/v1.0.0/property / detail ? address1 = 4529 % 20Winona % 20Court & address2=Denver % 2C % 20CO")
-// //     .get()
-// //     .addHeader("accept", "application/json")
-// //     .addHeader("apikey", "")
+// Selectors
 
-// //     .build();
+var searchMainEl = document.querySelector("#search-main");
+var searchMainBtnEl = document.querySelector("#search-main-btn");
+var mapEl = document.querySelector("#map");
 
-// // Response response = client.newCall(request).execute();
+var nutChartEl = document.getElementById("myNutChart");
+var barChartEl = document.getElementById("myBarChart");
+var lineChartEl = document.getElementById("myLineChart");
 
-// // var requestUrl = 'https://api.gateway.attomdata.com/propertyapi/v1.0.0'
+var marketInfoEl = document.getElementById("market-info");
+var educationInfoEl = document.getElementById("education-info");
 
-// // fetch(requestUrl)
-// //     .then()
+var weatherApiKey = "424b38ba3001e9ed90818dd50436deee";
 
-// //* go to url desired
-// //* look at documentation require for property
-// //* enter either url once or url and property multiple times (e.g. atom)
+// Event Listeners
+
+searchMainBtnEl.addEventListener("click", function () {
+  mapEl.classList.remove("d-none");
+});
+searchMainBtnEl.addEventListener("click", function () {
+  nutChartEl.classList.remove("d-none");
+});
+searchMainBtnEl.addEventListener("click", function () {
+  barChartEl.classList.remove("d-none");
+});
+searchMainBtnEl.addEventListener("click", function () {
+  lineChartEl.classList.remove("d-none");
+});
+searchMainBtnEl.addEventListener("click", function () {
+  marketInfoEl.classList.remove("d-none");
+});
+searchMainBtnEl.addEventListener("click", function () {
+  educationInfoEl.classList.remove("d-none");
+});
+
+// Google Maps API
+
+let map;
+var longitude = -74.0;
+var latitude = 40.71;
+
+function initMap() {
+  map = new google.maps.Map(document.getElementById("map"), {
+    center: { lat: latitude, lng: longitude },
+    zoom: 13,
+  });
+}
+
+window.initMap = initMap;
+
+// Weather API
+
+fetch(
+  "https://api.openweathermap.org/data/2.5/forecast?lat=" +
+    latitude +
+    "&lon=" +
+    longitude +
+    "&appid=" +
+    weatherApiKey,
+  {
+    method: "GET",
+  }
+)
+  .then((res) => res.json())
+  .then((data) => {
+    console.log("Successful POST request:", data);
+    return data;
+  })
+  .catch((error) => {
+    console.error("Error in POST request:", error);
+  });
+
+// Charts and Data Displays
+
+new Chart(nutChartEl, {
+  type: "doughnut",
+  data: {
+    labels: ["Red", "Blue", "Yellow", "Green", "Purple", "Orange"],
+    datasets: [
+      {
+        label: "# of Votes",
+        data: [12, 19, 3, 5, 2, 3],
+        borderWidth: 1,
+      },
+    ],
+  },
+  options: {
+    scales: {
+      y: {
+        beginAtZero: true,
+      },
+    },
+  },
+});
+
+new Chart(barChartEl, {
+  type: "bar",
+  data: {
+    labels: ["Red", "Blue", "Yellow", "Green", "Purple", "Orange"],
+    datasets: [
+      {
+        label: "# of Votes",
+        data: [12, 19, 3, 5, 2, 3],
+        borderWidth: 1,
+      },
+    ],
+  },
+  options: {
+    scales: {
+      y: {
+        beginAtZero: true,
+      },
+    },
+  },
+});
+
+new Chart(lineChartEl, {
+  type: "line",
+  data: {
+    labels: ["Red", "Blue", "Yellow", "Green", "Purple", "Orange"],
+    datasets: [
+      {
+        label: "# of Votes",
+        data: [12, 19, 3, 5, 2, 3],
+        borderWidth: 1,
+      },
+    ],
+  },
+  options: {
+    scales: {
+      y: {
+        beginAtZero: true,
+      },
+    },
+  },
+});
+
+// ATTOM API
 
 // fetch('https://api.gateway.attomdata.com/propertyapi/v1.0.0/property/id?geoid=PL0820000&minBeds=1&maxBeds=2', {
 //     method: 'GET',
@@ -51,6 +172,7 @@
 //         console.error('Error in POST request:', error);
 //     });
 
+// // ATTOM Fetch request
 // fetch('https://api.gateway.attomdata.com/propertyapi/v1.0.0/property/address?postalcode=82009&propertytype=RESIDENTIAL%20(NEC)&orderby=calendardate&page=1&pagesize=100', {
 //     method: 'GET',
 //     headers: {
@@ -75,58 +197,6 @@
 //     .catch((error) => {
 //         console.error('Error in POST request:', error);
 //     });
-
-// var container = document.querySelector(".container");
-
-// fetch('https://api.gateway.attomdata.com/propertyapi/v1.0.0/property/address?postalcode=82009&propertytype=RESIDENTIAL%20(NEC)&orderby=calendardate&page=1&pagesize=100', {
-//     method: 'GET',
-//     headers: {
-//         'Accept': 'application/json',
-//         'Content-Type': 'application/json',
-//         'apikey': 'f274e5cbf40a2264853a5e68d2b0d5d2'
-//     }
-// })
-//     .then((res) => res.json())
-//     .then((data) => {
-//         for (var i = 0; i < data.property.length; i++) {
-//             var createTableRow = document.createElement("tr");
-//             var tableData = document.createElement("td");
-//             var addressEl = document.createElement('p')
-
-//             console.log(data.property[i].address);
-//             var propertyAddressLine1 = data.property[i].address.line1;
-//             var propertyAddressLine2 = data.property[i].address.line2;
-//             console.log(propertyAddressLine1 + propertyAddressLine2);
-//             var address = propertyAddressLine1 + propertyAddressLine2;
-//             // var pEl = document.createElement("p");
-//             // pEl.textContent = address
-//             // container.append(pEl);
-//             addressEl.textContent = address;
-
-//             tableData.appendChild(addressEl);
-//             createTableRow.appendChild(tableData);
-//             container.appendChild(createTableRow);
-//             //var propertyStreetNumber =
-//             // for (var i = 0; i < property.length; i++) {
-//             //     console.log('Successful POST request:', property[i].address);
-//             // }
-//         }
-//         return data;
-//     })
-//     .catch((error) => {
-//         console.error('Error in POST request:', error);
-//     });
-
-function searchFunction() {
-  let value = document.querySelector("#search-value").value;
-  cityValuesSearch(value);
-}
-
-document.querySelector(".searchBtn").addEventListener("click", searchFunction);
-
-function cityValuesSearch(city) {
-  fetch;
-}
 
 // //* put arrays into a variable
 
